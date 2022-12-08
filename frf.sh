50a5548253b4ef3e7412b199bcf5f5043aab5be2
Teth-IO <105724285+Teth-IO@users.noreply.github.com>
2022-12-08 16:03:17 +0100

    init

---
+++ frf.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+
+# while-menu-dialog: a menu driven system information program
+
+DIALOG_CANCEL=1
+DIALOG_ESC=255
+HEIGHT=0
+WIDTH=0
+
+display_result() {
+  dialog --title "$1" \
+    --no-collapse \
+    --msgbox "$result" 0 0
+}
+
+while true; do
+  exec 3>&1
+  selection=$(dialog \
+    --backtitle "Bienvenue dans SemaOS" \
+    --title "Menu" \
+    --clear \
+    --cancel-label "Exit" \
+    --menu "Please select:" $HEIGHT $WIDTH 4 \
+    "1" "Mon IP privé" \
+    "2" "Mon IP publique" \
+    "3" "Scan de réseau" \
+    "4" "Test de connexion" \
+    "5" "Test de débit" \
+    2>&1 1>&3)
+  exit_status=$?
+  exec 3>&-
+  case $exit_status in
+    $DIALOG_CANCEL)
+      clear
+      echo "Program terminated."
+      exit
+      ;;
+    $DIALOG_ESC)
+      clear
+      echo "Program aborted." >&2
+      exit 1
+      ;;
+  esac
+  case $selection in
+    1 )
+      result=$(ip a)
+      display_result "Détail par interfaces"
+      ;;
+    2 )
+      result=$(curl wtfismyip.com/json)
+      display_result "IP Publique"
+      ;;
+    3 )
+      $(ip route | grep src | grep eth0 | awk '{print $1}' > tmp)
+      result=$(nmap -sn -iL tmp)
+      display_result "Scan du réseau"
+      ;;
+    4 )
+      result=$(less log)
+      display_result "Log de la santé de la connexion"
+      ;;
+    5 )
+      result=$(speedtest.cli)
+      display_result "Speedtest"
+  esac
+done
+# $(ip route | grep src | grep eth0 | awk '{print $1}' > tmp)
